cef48d9a5599c4832525f098172af83d24e98304
princekr96 <princekr700@gmail.com>
2022-01-09 19:23:03 +0530

    added rootmargin of 200px for image lazy loading

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -229,11 +229,9 @@ allSections.forEach(function (section) {
 
 /****************** Lazy Loading Images ******************/
 const imgTargets = document.querySelectorAll('img[data-src]');
-console.log('imgTargets', imgTargets);
 
 const loadImg = function (entries, observer) {
   const [entry] = entries;
-  console.log(entry);
 
   if (!entry.isIntersecting) return;
 
@@ -250,6 +248,7 @@ const loadImg = function (entries, observer) {
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
   threshold: 0,
+  rootMargin: '200px',
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
