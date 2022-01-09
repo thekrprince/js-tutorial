998318aae87a643f3217f9d446064eb92a518fe3
princekr96 <princekr700@gmail.com>
2022-01-09 15:41:19 +0530

    Reveal elements on scroll JS logic completed

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -207,9 +207,20 @@ const headerObserver = new IntersectionObserver(stickyNav, {
 /*************** Reveal Sections *************/
 const allSections = document.querySelectorAll('.section');
 
-const revealSection = function (entries, observer) {};
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
 
-const sectionObserver = new IntersectionObserver(revealSection, {});
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
 
 allSections.forEach(function (section) {
   sectionObserver.observe(section);
